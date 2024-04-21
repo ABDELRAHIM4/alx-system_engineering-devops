3711db5978a760d3c8f8f0efc9c1a87c459a34ff
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-21 15:37:16 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,10 @@
 #Puppet to make changes to our configuration file.
-file { '/etc/ssh/ssh_config':
-ensure  => present,
-content => 'PasswordAuthentication no\ndentityFile ~/.ssh/school',
+file_line {'Turn off Passed auth':
+path    =>'/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no',
+#require => package['openssh-client'],
+}
+file_line {'Declare identity file':
+path    =>'/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school',
 }
