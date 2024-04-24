c6689333c57557d0f0f4cf299d0926f6d0401096
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-04-24 15:36:03 -0700

    ssh

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,3 +4,11 @@ package { 'nginx':
 ensure   => present,
 provider => 'apt',
 }
+file {'/etc/nginx/sites-available/default':
+  ensure    => file,
+  content  => "server {
+  	listen 80 default_server;
+	location / {
+	return 200 "Hello World!";
+}
+}"
