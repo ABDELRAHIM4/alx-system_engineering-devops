4d5e7f9d5f36e497a7354231b8105a906d217cfb
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-05-20 16:29:48 -0700

    devops

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a Python script that, using this REST API"""
+import sys
+import csv
+import requests
+if __name__ == "__main__":
+    emp = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    res = requests.get(url + "users/{}".format(emp))
+    response = res.json()
+    params = {"userId" : emp}
+    todo = requests.get(url + "todos", params=params)
+    todo_js = todo.json()
+    com = []
+    for task in todo_js:
+        if task.get('completed') is  True:
+            com.append(task.get('title'))
+    with open("{}.csv".format(emp), 'w') as f:
+        form = ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]
+        writer = csv.DictWriter(f, fieldnames=form)
+        writer.writeheader()
+        #print(writer)
+        for task in todo_js:
+            writer.writerow({"USER_ID": emp, "USERNAME" : response.get("name"), "TASK_COMPLETED_STATUS" : task.get('completed'), "TASK_TITLE" : task.get('title')})
