201d3ede86deaae1cd48d75e13e378659d8fdec4
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-05-20 16:54:38 -0700

    devops

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,4 +21,4 @@ if __name__ == "__main__":
         writer.writeheader()
         #print(writer)
         for task in todo_js:
-            writer.writerow({"USER_ID": emp, "USERNAME" : response.get("name"), "TASK_COMPLETED_STATUS" : task.get('completed'), "TASK_TITLE" : task.get('title')})
+            writer.writerow({"USER_ID": "{}".format(str(emp)), "USERNAME" : "{}".format(str(response.get("name"))), "TASK_COMPLETED_STATUS" : "{}".format(str(task.get('completed'))), "TASK_TITLE" : str(task['title'])})
