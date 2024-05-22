7847e9e0ed7a3513489e94b46766363db36e0818
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-05-22 10:47:24 -0700

    devops

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     res = requests.get(url + "users/{}".format(emp))
     response = res.json()
-    params = {"userId" : emp}
+    params = {"userId": emp}
     todo = requests.get(url + "todos", params=params)
     todo_js = todo.json()
     com = []
     for task in todo_js:
-        if task.get('completed') is  True:
+        if task.get('completed') is True:
             com.append(task.get('title'))
     print("Employee {} is done with tasks({}/{}):".format(response.get("name"), len(com), len(todo_js)))
     for task in todo_js:
-        if task.get('completed') is  True:
+        if task.get('completed') is True:
             print("  {}".format(task.get('title')))
