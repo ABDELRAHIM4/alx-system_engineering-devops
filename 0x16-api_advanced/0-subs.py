f4cc724fab7383fdfd7775077ea00759c86955de
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-06-04 14:55:57 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Write a function that queries the Reddit API"""
 import requests
+
+
 def number_of_subscribers(subreddit):
     """Write a function that queries the Reddit API"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/".format(subreddit)
     headers = {"User-Agent": "user-agent"}
     res = requests.get(url, headers=headers, allow_redirects=False)
-    print("statuscode {}".format(res.status_code))
     if res.status_code == 200:
         data = res.json().get("data")
-        return data.get("subscribers")
+        return (data.get("subscribers"))
     else:
         return (0)
