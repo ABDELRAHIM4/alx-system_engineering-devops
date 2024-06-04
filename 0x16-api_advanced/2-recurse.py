6fc92221ec868f08bb8256a27995d54f9b8e6ed8
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-06-04 14:37:03 -0700

    api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """returns a list containing the titles of all hot articles"""
 import requests
+
+
 def recurse(subreddit, hot_list=[]):
     """returns a list containing the titles of all hot articles"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent":"useragent"
+        "User-Agent": "useragent"
     }
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
