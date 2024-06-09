5c85eb00cc2bc54e6fcb5fe730f912cd7714e9a3
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-06-09 13:05:04 +0000

    devops

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Using strace, find out why Apache is returning a 500 error.
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php && echo "" >> /root/alx-system_engineering-devops/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/usr/local/bin', '/bin'],
 }
-
