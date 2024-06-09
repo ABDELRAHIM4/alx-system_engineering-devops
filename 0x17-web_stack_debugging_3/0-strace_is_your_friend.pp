e62eb510eb3ff1a060a159a56e9926c01e3984eb
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-06-09 12:46:58 +0000

    devops

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,3 +3,8 @@ exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/use/local/bin/:/bin/'
 }
+file_line { 'append_newline':
+  path  => '/root/alx-system_engineering-devops/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp',
+  line  => '',
+  match => '\n$',
+}
