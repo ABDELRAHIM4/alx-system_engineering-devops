f4406209d252b2d6c1550d0a0eb7d58af578632a
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-06-09 12:54:35 +0000

    devops

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
-# Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
+# Using strace, find out why Apache is returning a 500 error.
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php && echo "" >> /root/alx-system_engineering-devops/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp',
   path    => ['/usr/local/bin', '/bin'],
 }
 
-file_line { 'append_newline':
-  path  => '/root/alx-system_engineering-devops/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp',
-  line  => '',
-  match => '\n$',
-}
